fa36b460b88f0e9202a27f9c5f6c475bdaf5a7bd
Gleb Litvinenko <miacross1001@gmail.com>
2023-01-08 07:42:13 -0800

    replace INSERT.sql

--- bd/INSERT.sql
+++ bd/INSERT.sql
@@ -55,19 +55,19 @@ insert into digest(name, issue) values
 insert into genreband(genre_id, band_id) values 
 	(1, 1),
 	(2, 2),
-	(2, 3),
-	(2, 4),
-	(3, 1),
-	(4, 1),
+	(3, 3),
+	(4, 4),
+	(5, 5),
+	(2, 6),
+	(3, 7),
+	(1, 8),
+	(2, 2),
 	(4, 3),
+	(5, 4),
 	(5, 1),
-	(5, 2),
-	(6, 1),
-	(6, 2),
-	(7, 1),
-	(8, 1),
-	(8, 2);
-
+	(1, 2),
+	(1, 8);
+	
 insert into bandalbums(band_id, albums_id) values 
 	(1, 8),
 	(2, 7),
@@ -94,11 +94,3 @@ insert into musicdigest(music_id, digest_id) values
 	(13, 6),
 	(14, 7),
 	(15, 8);
-
-
-
-
-
-
-
-	
